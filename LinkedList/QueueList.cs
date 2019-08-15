8108c02d800528222df1ce2a52e9567dce994a36
aarpi <arpiarustamyan21@gmail.com>
2019-08-16 01:53:01 +0400

    Add StackList QueueList

---
+++ LinkedList/QueueList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LinkedList.LinkedList;
+
+namespace LinkedList
+{
+    public class QueueList
+    {
+        private LinkedList list;
+
+        public void Enqueue(int value)
+        {
+            list.AddLast(value);
+        }
+        public int Dequeue()
+        {
+            return list.RemoveFirst();
+        }
+        public bool IsEmpty()
+        {
+            return list.IsEmpty();
+        }
+        public int Peek()
+        {
+            return list.GetFirst();
+        }
+        public int Count()
+        {
+            return list.Count;
+        }
+        public void Reverce()
+        {
+            list.Reverce();
+        }
+    }
+}
---
+++ LinkedList/StackList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LinkedList.LinkedList;
+
+namespace LinkedList
+{
+    public class StackList
+    {
+        private LinkedList list;
+
+        public void Push(int value)
+        {
+            list.AddFirst(value);
+        }
+        public int Pop()
+        {
+            return list.RemoveFirst();
+        }
+        public bool IsEmpty()
+        {
+            return list.IsEmpty();
+        }
+        public int Peek()
+        {
+            return list.GetFirst();
+        }
+        public int Count()
+        {       
+            return list.Count;
+        }
+        public void Reverce()
+        {
+            list.Reverce();
+        }
+    }
+}
