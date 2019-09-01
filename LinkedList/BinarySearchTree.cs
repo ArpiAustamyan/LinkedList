1b71070e36f576edfab7693d1f8a411fa0c0809f
arpi <arpiarustamyan21@gmail.com>
2019-09-01 10:39:09 +0400

    Add BinarySearchTree

---
+++ LinkedList/BinarySearchTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class BinarySearchTree:BinaryTree
+    {
+        public new void Add(int value)  
+        {
+            NodeTree newItem = new NodeTree(value);
+            base.Add(newItem);
+            if (newItem.parent != null && newItem.parent.parent != null)
+            {
+                NodeTree ptr = newItem.parent.parent;
+                if (ptr.left.bal - ptr.right.bal >= 2 || ptr.left.bal - ptr.right.bal <= -2)
+                    Balance(newItem);
+            }
+        }
+        public void Balance(NodeTree item)
+        {
+            NodeTree ptr = item.parent.parent;
+            {
+                item.parent = ptr.parent;
+                ptr.parent.left = item;
+            }
+        }
+    }
+}
---
+++ LinkedList/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedListPerson
+    {
+        public class Person
+        {
+            private string name;
+            private string surname;
+        }
+        //private LinkedList<Person> persons;
+
+        //public void Show()
+        //{
+        //    persons.Show();
+        //}
+    }
+}
