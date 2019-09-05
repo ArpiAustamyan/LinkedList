4284ca7a188f9d8bf2a37309582ce9680321ac4f
arpi <arpiarustamyan21@gmail.com>
2019-09-05 14:57:39 +0400

    Sorting Algoritms

---
+++ LinkedList/Sorting.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public static class Sorting
+    {
+        public static int[] BubbleSort(int[] arr)
+        {
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                for (int i = 1; i < arr.Length - 1 - j; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        public static int[] InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                int val = arr[i];
+                int flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+            return arr;
+        }
+
+        public static int[] SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int index = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[index])
+                    {
+                        index = j;
+                    }
+                }
+                int temp = arr[index];
+                arr[index] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    QuickSort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    QuickSort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        public static int[] CountingSort(int[] array)
+        {
+            int[] sortedArray = new int[array.Length];
+
+            int minVal = array[0];
+            int maxVal = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minVal) minVal = array[i];
+                else if (array[i] > maxVal) maxVal = array[i];
+            }
+
+            int[] counts = new int[maxVal - minVal + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                counts[array[i] - minVal]++;
+            }
+
+            counts[0]--;
+            for (int i = 1; i < counts.Length; i++)
+            {
+                counts[i] = counts[i] + counts[i - 1];
+            }
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                sortedArray[counts[array[i] - minVal]--] = array[i];
+            }
+
+            return sortedArray;
+        }
+
+    }
+}
